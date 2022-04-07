6733c054c112b6bfea0d6d5b3330fb09ef95e5c4
Smccb <91168013+Smccb@users.noreply.github.com>
2022-04-07 20:21:11 +0100

    Update LaptopComputer.java

--- Lab6Part3/src/LaptopComputer.java
+++ Lab6Part3/src/LaptopComputer.java
@@ -3,5 +3,11 @@
 public class LaptopComputer extends Computer {
 	int batteryLife;
 	
+	public LaptopComputer() {
+		
+	}
 	
+	public LaptopComputer(int batteryLife) {
+			this.batteryLife = batteryLife;
+		}
 }
