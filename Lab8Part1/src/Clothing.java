48a238c8de7d866fc96ebe90c9068e801370fddc
Smccb <sarahmmccb@gmail.com>
2022-04-08 02:12:59 +0100

    lab8Part1

---
+++ Lab8Part1/src/Clothing.java
@@ -0,0 +1,21 @@
+
+public class Clothing {
+	private String designer;
+	private String colour;
+	private String material;
+	
+	public Clothing() {
+		
+	}
+	
+	public Clothing(String designer) {
+		this.designer = designer;
+	}
+	
+	public Clothing(String designer, String colour, String material) {
+		this.designer = designer;
+		this.colour = colour;
+		this.material = material;
+	}
+	
+}
---
+++ Lab8Part1/src/TestLab8Part1.java
@@ -0,0 +1,6 @@
+
+public class TestLab8Part1 {
+	public static void main(String [] args) {
+		Clothing clothing = new Clothing();
+	}
+}
