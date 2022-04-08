239075f8afa24ae2658cfba2f645e52591658cf2
Smccb <sarahmmccb@gmail.com>
2022-04-08 05:02:45 +0100

    Update Clothing.java

--- Lab8Part1/src/Clothing.java
+++ Lab8Part1/src/Clothing.java
@@ -4,18 +4,22 @@ public class Clothing {
 	private String colour;
 	private String material;
 	
+	//default constructor
 	public Clothing() {
 		
 	}
 	
+	//constructor passing only one item
 	public Clothing(String designer) {
 		this.designer = designer;
 	}
 	
+	//constructor passing two attributes
 	public Clothing(String designer, String colour) {
 		this.designer = designer;
 	}
 	
+	//constructor passing all attributes
 	public Clothing(String designer, String colour, String material) {
 		this.designer = designer;
 		this.colour = colour;
